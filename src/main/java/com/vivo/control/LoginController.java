a458c9113465c371664a13b02edb402c5f655e53
leipengfei <1196327324@qq.com>
2019-09-10 21:58:21 +0800

    配置拦截器

---
+++ src/main/java/com/vivo/control/LoginController.java
@@ -0,0 +1,27 @@
+package com.vivo.control;
+
+import com.vivo.service.PersonService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
+
+@Controller
+public class LoginController {
+
+    @Resource(name = "personService")
+    private PersonService personService;
+
+    @GetMapping(value = "login")
+    public String login(Integer id, String password, HttpSession session){
+        System.out.println(personService.getByid(id));
+        if (password.equals(personService.getByid(id).getPassword())){
+            session.setAttribute("person",personService.getByid(id));
+            return "redirect:person/getAll";
+        }
+        return  "redirect:login.jsp";
+    }
+}
